8f13fbda896e2dcf8f19554b7dd2f39459e13bd4
jgunstone <j.gunstone@maxfordham.com>
2025-04-01 12:24:17 +0100

    ruff

--- src/bdns_plus/iref_io.py
+++ src/bdns_plus/iref_io.py
@@ -1,6 +1,8 @@
-from annotated_types import Ge, Annotated
-import typing as ty
+from __future__ import annotations
+
 import logging
+
+from annotated_types import Annotated, Ge
 
 logger = logging.getLogger(__name__)
 level_min, level_max, ground = -9, 89, 90
@@ -9,10 +11,9 @@ level_min, level_max, ground = -9, 89, 90
 def get_level_id(level: int):
     if level == 0:
         return ground
-    elif level < 0:
+    if level < 0:
         return ground - level
-    else:
-        return level
+    return level
 
 
 def gen_levels(low, high):
@@ -20,18 +21,23 @@ def gen_levels(low, high):
 
 
 def serialize_iref(
-    level_number: int, level_iref: Annotated[int, Ge(0)], level_min=-9, level_max=89
+    level_number: int,
+    level_iref: Annotated[int, Ge(0)],
+    level_min=-9,
+    level_max=89,
 ) -> Annotated[int, Ge(0)]:
-    """return instance reference integer (>0) given a level number and level instance"""
+    """Return instance reference integer (>0) given a level number and level instance"""
     map_level_id = gen_levels(level_min, level_max)
     level_id = map_level_id[level_number]
     return int(f"{level_id}0{level_iref}")
 
 
 def deserialize_iref(
-    iref: Annotated[int, Ge(0)], level_min=-9, level_max=89
-) -> tuple[ty.Union[int, None], ty.Union[Annotated[int, Ge(0)], None]]:
-    """return a level number and level instance given a instance reference integer (>0)"""
+    iref: Annotated[int, Ge(0)],
+    level_min=-9,
+    level_max=89,
+) -> tuple[int | None, Annotated[int, Ge(0)] | None]:
+    """Return a level number and level instance given a instance reference integer (>0)"""
     try:
         level_id, level_iref = str(iref).split("0", 1)
     except ValueError:
@@ -46,25 +52,23 @@ def deserialize_iref(
     return level_number, level_iref
 
 
-def get_next_iref(irefs: list[int], level_number: ty.Union[None, int] = None):
+def get_next_iref(irefs: list[int], level_number: None | int = None):
     if level_number is None:
         return max(irefs) + 1
-    elif level_number is not None and len(irefs) == 0:
+    if level_number is not None and len(irefs) == 0:
         return serialize_iref(level_number, 1)
-    elif level_number is not None and len(irefs) > 0:
+    if level_number is not None and len(irefs) > 0:
         _ = [deserialize_iref(iref) for iref in irefs]
         level_irefs = [x[1] for x in _ if x[0] == level_number]
         if len(level_irefs) == 0:
             return serialize_iref(level_number, 1)
-        else:
-            return serialize_iref(level_number, max(level_irefs) + 1)
-    else:
-        e = f"get_next_iref error where irefs={irefs}, level_number={level_number}"
-        raise ValueError(e)
+        return serialize_iref(level_number, max(level_irefs) + 1)
+    e = f"get_next_iref error where irefs={irefs}, level_number={level_number}"
+    raise ValueError(e)
 
 
 def check_dict_var(data: dict, key: str):
-    if key in data.keys():
+    if key in data:
         if data[key] is not None:
             return True
     else:
@@ -73,15 +77,13 @@ def check_dict_var(data: dict, key: str):
 
 def is_level_number_and_level_instance_reference(
     data: dict,
-) -> ty.Union[None, tuple[int, int]]:
-    """return values for `level_number` and `level_instance_reference`
-    if they exist and are not None"""
+) -> None | tuple[int, int]:
+    """Return values for `level_number` and `level_instance_reference`
+    if they exist and are not None
+    """
     if False in [
         check_dict_var(data, "level_number"),
         check_dict_var(data, "level_instance_reference"),
     ]:
         return False
-    else:
-        return True
-
-
+    return True
